7ac2bede80642cde216d628b5053a094f1075a63
mrizwan1122 <gujjar19014@gmail.com>
2023-04-01 03:37:32 +0500

    changes in website more in pricing plan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -328,51 +328,51 @@
 
           <div class="col-lg-3 col-md-6">
             <div class="box" data-aos="zoom-in">
-              <h3>Free</h3>
-              <h4><sup>$</sup>0<span> / month</span></h4>
+              <h3>Starter</h3>
+              <h4><sup>Rs.</sup>5,000<span> / month</span></h4>
               <ul>
-                <li>Aida dere</li>
-                <li>Nec feugiat nisl</li>
-                <li>Nulla at volutpat dola</li>
-                <li class="na">Pharetra massa</li>
-                <li class="na">Massa ultricies mi</li>
+                <li>Advance payment</li>
+                <li>Hosting</li>
+                <li class="na">Maintenence</li>
+                <li class="na">SMS service</li>
+                <li class="na">Secure backup</li>
               </ul>
               <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
+                <a href="#" class="btn-buy">Check in</a>
               </div>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-6 mt-4 mt-md-0">
             <div class="box featured" data-aos="zoom-in" data-aos-delay="100">
-              <h3>Business</h3>
-              <h4><sup>$</sup>19<span> / month</span></h4>
+              <h3>Basic</h3>
+              <h4><sup>Rs.</sup>10,000<span> / month</span></h4>
               <ul>
-                <li>Aida dere</li>
-                <li>Nec feugiat nisl</li>
-                <li>Nulla at volutpat dola</li>
-                <li>Pharetra massa</li>
-                <li class="na">Massa ultricies mi</li>
+                <li>Advance payment</li>
+                <li>Hosting</li>
+                <li class="na">Maintenence</li>
+                <li class="na">SMS service</li>
+                <li>Secure backup</li>
               </ul>
               <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
+                <a href="#" class="btn-buy">Check in</a>
               </div>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-6 mt-4 mt-lg-0">
             <div class="box" data-aos="zoom-in" data-aos-delay="200">
-              <h3>Developer</h3>
-              <h4><sup>$</sup>29<span> / month</span></h4>
+              <h3>Standard</h3>
+              <h4><sup>Rs.</sup>20,000<span> / month</span></h4>
               <ul>
-                <li>Aida dere</li>
-                <li>Nec feugiat nisl</li>
-                <li>Nulla at volutpat dola</li>
-                <li>Pharetra massa</li>
-                <li>Massa ultricies mi</li>
+                <li>Advance payment</li>
+                <li>Hosting</li>
+                <li>Maintenence</li>
+                <li class="na">SMS service</li>
+                <li class="na">Secure backup</li>
               </ul>
               <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
+                <a href="#" class="btn-buy">Check in</a>
               </div>
             </div>
           </div>
@@ -380,17 +380,17 @@
           <div class="col-lg-3 col-md-6 mt-4 mt-lg-0">
             <div class="box" data-aos="zoom-in" data-aos-delay="300">
               <span class="advanced">Advanced</span>
-              <h3>Ultimate</h3>
-              <h4><sup>$</sup>49<span> / month</span></h4>
+              <h3>Premium</h3>
+              <h4><sup>Rs.</sup>30,000<span> / month</span></h4>
               <ul>
-                <li>Aida dere</li>
-                <li>Nec feugiat nisl</li>
-                <li>Nulla at volutpat dola</li>
-                <li>Pharetra massa</li>
-                <li>Massa ultricies mi</li>
+                <li class="na">Advance payment</li>
+                <li>Hosting</li>
+                <li>Maintenence</li>
+                <li>SMS service</li>
+                <li>Secure backup</li>
               </ul>
               <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
+                <a href="#" class="btn-buy">Check in</a>
               </div>
             </div>
           </div>
