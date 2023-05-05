6b4e1fc0a2b56c9d45732278d14a641289fcdb25
mrizwan1122 <gujjar19014@gmail.com>
2023-05-05 10:31:36 +0500

    add some team members

--- resources/views/sections/team-section.blade.php
+++ resources/views/sections/team-section.blade.php
@@ -21,6 +21,42 @@
           </div>
 
           <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up">
+              <div class="member-img">
+                <img src="{{asset('assets/img/team/umar.jpeg')}}" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Umar Tarar</h4>
+                <span>ACA ERP Lead</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up">
+              <div class="member-img">
+                <img src="{{asset('assets/img/team/nauman.jpeg')}}" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Nauman Aslam</h4>
+                <span>BDM</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up">
+              <div class="member-img">
+                <img src="{{asset('assets/img/team/shoib.jpeg')}}" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Muhammad Shoib</h4>
+                <span>Developer</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
             <div class="member" data-aos="fade-up" data-aos-delay="200">
               <div class="member-img">
                 <img src="{{asset('assets/img/team/rizwan.jpeg')}}" class="img-fluid" alt="">
@@ -40,6 +76,18 @@
               <div class="member-info">
                 <h4>Hassan Fayyaz</h4>
                 <span>Developer</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="200">
+              <div class="member-img">
+                <img src="{{asset('assets/img/team/sidra.jpeg')}}" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Sidra Latif</h4>
+                <span>QA Specialist</span>
               </div>
             </div>
           </div>
