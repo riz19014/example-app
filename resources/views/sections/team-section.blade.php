5c201a6055e09eaf15294da3ba8b3e08d27329ca
riz19014 <rizwan19014@gmail.com>
2025-04-17 21:28:52 +0500

    website solution als added

--- resources/views/sections/team-section.blade.php
+++ resources/views/sections/team-section.blade.php
@@ -1,113 +1,26 @@
 <section id="team" class="team">
-      <div class="container">
+  <div class="container">
 
-        <div class="section-title" data-aos="zoom-out">
-          <h2>Testimonials</h2>
-          <p>Our TRUSTED CLIENTS</p>
-        </div>
+    <div class="section-title" data-aos="zoom-out">
+      <h2>Testimonials</h2>
+      <p>Our TRUSTED CLIENTS</p>
+    </div>
 
+    <!-- Tabs for Categories -->
+    <ul class="nav nav-tabs mb-4" id="testimonialTabs" role="tablist">
+      <li class="nav-item">
+        <a class="nav-link active" id="apps-tab" data-bs-toggle="tab" href="#apps" role="tab">Business Software Applications</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="web-tab" data-bs-toggle="tab" href="#web" role="tab">Website Applications</a>
+      </li>
+    </ul>
+
+    <div class="tab-content" id="testimonialTabsContent">
+
+      <!-- Business Applications -->
+      <div class="tab-pane fade show active" id="apps" role="tabpanel">
         <div class="row">
-
-          {{--<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up">
-              <div class="member-img">
-                <img width="400" height="500" src="{{asset('assets/img/team/tariq.jpg')}}" class="img-fluid" alt="">
-              </div>
-              <div class="member-info">
-                <h4>Tariq Naeem</h4>
-                <span>Project Manager</span>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up">
-              <div class="member-img">
-                <img width="400" height="500" src="{{asset('assets/img/team/umar.jpeg')}}" class="img-fluid" alt="">
-              </div>
-              <div class="member-info">
-                <h4>Umar Tarar</h4>
-                <span>ACA ERP Lead</span>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up">
-              <div class="member-img">
-                <img width="400" height="500" src="{{asset('assets/img/team/nauman.jpeg')}}" class="img-fluid" alt="">
-              </div>
-              <div class="member-info">
-                <h4>Nauman Aslam</h4>
-                <span>BDM</span>
-              </div>
-            </div>
-          </div>
-
-
-          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up" data-aos-delay="200">
-              <div class="member-img">
-                <img width="400" height="500" src="{{asset('assets/img/team/file.enc')}}" class="img-fluid" alt="">
-              </div>
-              <div class="member-info">
-                <h4>Muhammad Ahmed</h4>
-                <span>Senior Developer(Consultant)</span>
-              </div>
-            </div>
-          </div>
-
-
-          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up">
-              <div class="member-img">
-                <img width="400" height="500" src="{{asset('assets/img/team/shoib.jpeg')}}" class="img-fluid" alt="">
-              </div>
-              <div class="member-info">
-                <h4>Muhammad Shoib</h4>
-                <span>Developer</span>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up" data-aos-delay="200">
-              <div class="member-img">
-                <img width="400" height="500" src="{{asset('assets/img/team/rizwan.jpeg')}}" class="img-fluid" alt="">
-              </div>
-              <div class="member-info">
-                <h4>Muhammad Rizwan</h4>
-                <span>CTO/Developer/Co-founder</span>
-              </div>
-            </div>
-          </div>
-
-          
-
-          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up" data-aos-delay="200">
-              <div class="member-img">
-                <img width="400" height="500" src="{{asset('assets/img/team/hassan.png')}}" class="img-fluid" alt="">
-              </div>
-              <div class="member-info">
-                <h4>Hassan Fayyaz</h4>
-                <span>Developer</span>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up" data-aos-delay="200">
-              <div class="member-img">
-                <img width="400" height="500" src="{{asset('assets/img/team/sidra.jpeg')}}" class="img-fluid" alt="">
-              </div>
-              <div class="member-info">
-                <h4>Sidra Latif</h4>
-                <span>QA Specialist</span>
-              </div>
-            </div>
-          </div>--}}
-
 
           <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
             <div class="member" data-aos="fade-up" style="text-align: center;">
@@ -116,9 +29,8 @@
               </div>
               <div class="member-info">
                 <h4>Kingway Group Of School</h4>
-                <span>Delivered</span>
-                <br>
-                    <a href="https://kws.mrtnsol.com/" target="_blank">Visit Kingway Group Of School</a>
+                <span>Delivered (School Management Software)</span><br>
+                <a href="https://kws.mrtnsol.com/" target="_blank">Visit Kingway Group Of School</a>
               </div>
             </div>
           </div>
@@ -130,26 +42,24 @@
               </div>
               <div class="member-info">
                 <h4>Mumtaz Grammar School</h4>
-                <span>Delivered</span>
-                <br>
-                    <a href="https://mgs.mrtnsol.com/" target="_blank">Visit Mumtaz Grammar School</a>
+                <span>Delivered (School ERP)</span><br>
+                <a href="https://mgs.mrtnsol.com/" target="_blank">Visit Mumtaz Grammar School</a>
               </div>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
             <div class="member" data-aos="fade-up" style="text-align: center;">
-                <div class="member-img">
-                    <img width="260" height="260" src="{{asset('assets/img/team/logo.svg')}}" class="img-fluid" alt="">
-                </div>
-                <div class="member-info">
-                    <h4>Nishat</h4>
-                    <span>Delivered</span>
-                    <br>
-                    <a href="javascript:void(0)">Visit Nishat</a>
-                </div>
+              <div class="member-img">
+                <img width="260" height="260" src="{{asset('assets/img/team/logo.svg')}}" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Nishat</h4>
+                <span>Delivered (HR & Payroll System)</span><br>
+                <a href="javascript:void(0)">Visit Nishat</a>
+              </div>
             </div>
-        </div>
+          </div>
 
           <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
             <div class="member" data-aos="fade-up" style="text-align: center;">
@@ -158,9 +68,8 @@
               </div>
               <div class="member-info">
                 <h4>Auto House</h4>
-                <span>Delivered</span>
-                <br>
-                    <a href="https://auto-house.mrtnsol.com/" target="_blank">Visit Auto House</a>
+                <span>Delivered (Inventory + POS System)</span><br>
+                <a href="https://auto-house.mrtnsol.com/" target="_blank">Visit Auto House</a>
               </div>
             </div>
           </div>
@@ -172,30 +81,68 @@
               </div>
               <div class="member-info">
                 <h4>Kinetic Lubricants</h4>
-                <span>Testing Phase</span>
-                <br>
-                    <a href="https://kinetic-lubricant.mrtnsol.com/" target="_blank">Visit Kinetic Lubricants</a>
+                <span>Testing Phase (CRM + Sales Tracking)</span><br>
+                <a href="https://kinetic-lubricant.mrtnsol.com/" target="_blank">Visit Kinetic Lubricants</a>
               </div>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
             <div class="member" data-aos="fade-up" style="text-align: center;">
-                <div class="member-img">
-                    <img width="260" height="260" src="{{asset('assets/img/team/abc2.png')}}" class="img-fluid" alt="">
-                </div>
-                <div class="member-info">
-                    <h4>MAL Pakistan</h4>
-                    <span>Development Phase</span>
-                    <br>
-                    <a href="https://mobil-dms.mrtnsol.com/" target="_blank">Visit MAL Pakistan</a>
-                </div>
+              <div class="member-img">
+                <img width="260" height="260" src="{{asset('assets/img/team/abc2.png')}}" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>MAL Pakistan</h4>
+                <span>Development Phase (Dealer Management System)</span><br>
+                <a href="https://mobil-dms.mrtnsol.com/" target="_blank">Visit MAL Pakistan</a>
+              </div>
             </div>
+          </div>
+
         </div>
-
+        <hr>
         
-
-
-
       </div>
-    </section>
+
+      <!-- Website Showcase -->
+      <div class="tab-pane fade" id="web" role="tabpanel">
+        <div class="row justify-content-center">
+
+          <div class="col-lg-5 col-md-6 d-flex align-items-stretch">
+  <div class="member" data-aos="fade-up">
+    <div class="member-img">
+      <img width="260" height="260" src="{{ asset('assets/img/team/kl.jpeg') }}" class="img-fluid" alt="KLS Logo">
+    </div>
+    <div class="member-info">
+      <h4>KLS Lubricants</h4>
+      <span>Corporate Website</span><br>
+      <a href="https://kls.com.pk/" target="_blank">Visit kls.com.pk</a>
+    </div>
+  </div>
+</div>
+
+
+          <div class="col-lg-5 col-md-6 d-flex align-items-stretch">
+  <div class="member" data-aos="fade-up">
+    <div class="member-img">
+      <img width="260" height="260" src="{{ asset('assets/img/team/plake.svg') }}" class="img-fluid" alt="Plake Logo">
+    </div>
+    <div class="member-info">
+      <h4>Plake.io</h4>
+      <span>E-commerce Websites</span><br>
+      <a href="https://plake.io/" target="_blank">Visit plake.io</a>
+    </div>
+  </div>
+</div>
+
+
+        </div>
+        <hr>
+  
+      </div>
+
+    </div>
+
+  </div>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
           <li><a class="nav-link scrollto" href="#about">About</a></li>
           <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
+          {{--<li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>--}}
           <li><a class="nav-link scrollto" href="#team">Testimonials</a></li>
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
         </ul>
@@ -160,9 +160,9 @@
 
     <!-- End Services Section -->
 
-    <!-- ======= Pricing Section ======= -->
+    <!-- ======= Pricing Section ======= 
 
-        @include('sections.pricing-section')
+        @include('sections.pricing-section')  -->
 
     <!-- End Pricing Section -->
 
